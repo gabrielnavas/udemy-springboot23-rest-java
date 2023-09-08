eb845cb2ca2debb67b0c9ad1058e5a3074464fe9
navas <gabrielnavas@email.com>
2023-09-08 01:54:38 -0300

    receives and produces xml or json on api via header

--- person/src/main/java/com/person/configs/WebConfig.java
+++ person/src/main/java/com/person/configs/WebConfig.java
@@ -10,10 +10,26 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//        configureContentNegotiationViaParam(configurer);
+        configureContentNegotiationViaHeader(configurer);
+    }
+
+
+    private void configureContentNegotiationViaParam(ContentNegotiationConfigurer configurer) {
         // Via Query Param. Example: http://localhost:8080/api/person/v1?mediaType=xml
         configurer.favorParameter(true) // accept param
                 .parameterName("mediaType") // name of the param
                 .ignoreAcceptHeader(true) // ignore param on the header
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON) // uses as default
+                .mediaType("json", MediaType.APPLICATION_JSON) // accept json
+                .mediaType("xml", MediaType.APPLICATION_XML); // accept xml
+    }
+
+    private void configureContentNegotiationViaHeader(ContentNegotiationConfigurer configurer) {
+        // Via Query Param. Example: http://localhost:8080/api/person/v1?mediaType=xml
+        configurer.favorParameter(false) // accept param
+                .ignoreAcceptHeader(false) // ignore param on the header
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON) // uses as default
                 .mediaType("json", MediaType.APPLICATION_JSON) // accept json
