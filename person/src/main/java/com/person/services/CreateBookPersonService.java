20ad9e12a148ebda41a6c46a78d1cb3a2280043f
navas <gabrielnavas@email.com>
2023-09-12 20:16:56 -0300

    verify if book already exists with title

--- person/src/main/java/com/person/services/CreateBookPersonService.java
+++ person/src/main/java/com/person/services/CreateBookPersonService.java
@@ -1,11 +1,13 @@
 package com.person.services;
 
 import com.person.dtos.CreateBookDto;
+import com.person.exceptions.ObjectAlreadyExistsWithException;
 import com.person.models.Book;
 import com.person.repositories.BookRepositoryJpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -15,6 +17,11 @@ public class CreateBookPersonService {
     private BookRepositoryJpa bookRepository;
 
     public Book execute(CreateBookDto dto) {
+        Optional<Book> bookByTitleFound = bookRepository.findByTitle(dto.getTitle());
+        if (bookByTitleFound.isPresent()) {
+            throw new ObjectAlreadyExistsWithException("book", "title");
+        }
+
         Book book = new Book(
                 UUID.randomUUID(),
                 dto.getTitle(),
