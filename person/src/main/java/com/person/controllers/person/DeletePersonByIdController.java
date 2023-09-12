43bde3af219371e189c3130077227cdadb5d8b23
navas <gabrielnavas@email.com>
2023-09-12 15:59:46 -0300

    added tag to controller and documentation of some types of returns in the delete a person execute method

--- person/src/main/java/com/person/controllers/person/DeletePersonByIdController.java
+++ person/src/main/java/com/person/controllers/person/DeletePersonByIdController.java
@@ -1,6 +1,10 @@
 package com.person.controllers.person;
 
 import com.person.services.DeletePerson;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +17,21 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
 public class DeletePersonByIdController {
     @Autowired
     private DeletePerson deletePerson;
 
     @DeleteMapping(value = "/{personId}")
+    @Operation(summary = "delete a people", description = "delete a people",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Object> execute(
             @PathVariable("personId") UUID personId
     ) {
