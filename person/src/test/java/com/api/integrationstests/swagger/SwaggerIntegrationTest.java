13c3cef1ed7c4ab4dbb9c5b7323100fbb0f61ec1
navas <gabrielnavas@email.com>
2023-09-28 18:00:29 -0300

    added containerstest and rest assured

---
+++ person/src/test/java/com/api/integrationstests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.api.integrationstests.swagger;
+
+import com.api.configs.TestConfigs;
+import com.api.integrationstests.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void shouldDisplaySwaggerUiPage() {
+        String content = given()
+                .basePath("/api/swagger-ui.html")
+                .port(TestConfigs.SERVER_PORT)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        assertTrue(content.contains("Swagger UI"));
+    }
+}
--- person/src/test/java/com/api/main/PersonApplicationTests.java
+++ person/src/test/java/com/api/main/PersonApplicationTests.java
@@ -1,4 +1,4 @@
-package com.api;
+package com.api.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PersonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
