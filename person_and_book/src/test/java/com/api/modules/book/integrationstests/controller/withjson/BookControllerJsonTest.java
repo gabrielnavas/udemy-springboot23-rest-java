64ce5c807eeaf7f5b400b6dd66445e793ec8c128
navas <gabrielnavas@email.com>
2023-09-29 19:14:39 -0300

    added integration test create book

---
+++ person_and_book/src/test/java/com/api/modules/book/integrationstests/controller/withjson/BookControllerJsonTest.java
@@ -0,0 +1,101 @@
+package com.api.modules.book.integrationstests.controller.withjson;
+
+import com.api.configs.TestConfigs;
+import com.api.modules.book.integrationstests.httpbody.CreateUpdatePartialsBookRequest;
+import com.api.modules.book.integrationstests.httpbody.ResponseBook;
+import com.api.testcontainers.AbstractIntegrationTest;
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BookControllerJsonTest extends AbstractIntegrationTest {
+
+
+    private static RequestSpecification specification;
+
+    private static ObjectMapper objectMapper;
+
+    private static CreateUpdatePartialsBookRequest bookFake;
+    private static ResponseBook responseBook;
+    private static Faker faker;
+
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES); //disable fails of the attributes without on object deserialization
+
+        bookFake = null;
+        responseBook = null;
+        faker = new Faker();
+    }
+
+
+    @Test
+    @Order(1)
+    void createBookTest() throws IOException {
+        bookFake = createNewBookRequest();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_LOCALHOST)
+                .setBasePath("/api/book/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        Response response = given().spec(specification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(bookFake)
+                .when()
+                .post()
+                .then()
+                .extract()
+                .response();
+
+        responseBook = objectMapper.readValue(response.body().asString(), ResponseBook.class);
+
+        assertEquals(HttpStatus.CREATED.value(), response.getStatusCode());
+
+        assertNotNull(responseBook);
+
+        assertNotNull(responseBook.getId());
+        assertNotNull(responseBook.getAuthor());
+        assertNotNull(responseBook.getLaunchDate());
+        assertNotNull(responseBook.getTitle());
+        assertNotNull(responseBook.getPrice());
+
+        assertEquals(bookFake.getAuthor(), responseBook.getAuthor());
+        assertEquals(bookFake.getLaunchDate(), responseBook.getLaunchDate());
+        assertEquals(bookFake.getTitle(), responseBook.getTitle());
+        assertEquals(bookFake.getPrice(), responseBook.getPrice());
+    }
+
+    private CreateUpdatePartialsBookRequest createNewBookRequest() {
+        CreateUpdatePartialsBookRequest bookFake = new CreateUpdatePartialsBookRequest();
+        bookFake.setAuthor(faker.book().author());
+        bookFake.setPrice(faker.random().nextDouble());
+        bookFake.setTitle(faker.book().title());
+        bookFake.setLaunchDate(faker.date().birthday());
+        return bookFake;
+    }
+}
---
+++ person_and_book/src/test/java/com/api/modules/person/integrationstests/httpbody/ResponsePerson.java
@@ -0,0 +1,117 @@
+package com.api.modules.person.integrationstests.httpbody;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class ResponsePerson implements Serializable {
+    private String id;
+
+    @JsonProperty(value = "first_name")
+    private String firstname;
+    @JsonProperty(value = "last_name")
+    private String lastname;
+    private Date birthday;
+    private String username;
+    private String email;
+    @JsonProperty(value = "created_at")
+    private Date createdAt;
+    @JsonProperty(value = "updated_at")
+    private Date updatedAt;
+
+    public ResponsePerson() {
+        this("", "", "", new Date(), "", "", new Date(), new Date());
+    }
+
+    public ResponsePerson(String id, String firstname, String lastname, Date birthday, String username, String email,
+                          Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthday = birthday;
+        this.username = username;
+        this.email = email;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ResponsePerson that = (ResponsePerson) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstname, that.firstname) && Objects.equals(lastname, that.lastname) && Objects.equals(birthday, that.birthday) && Objects.equals(username, that.username) && Objects.equals(email, that.email) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstname, lastname, birthday, username, email, createdAt, updatedAt);
+    }
+}
+
