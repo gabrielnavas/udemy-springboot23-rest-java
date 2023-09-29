eb2dfa4b4305289c7320360b0d4a25f14227e715
navas <gabrielnavas@email.com>
2023-09-29 01:42:45 -0300

    first teste http integration create person

--- person_and_book/src/test/java/com/api/configs/TestConfigs.java
+++ person_and_book/src/test/java/com/api/configs/TestConfigs.java
@@ -3,4 +3,7 @@ package com.api.configs;
 public class TestConfigs {
 
     public static final int SERVER_PORT = 8888;
+
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
 }
---
+++ person_and_book/src/test/java/com/api/integrationstests/controller/withjson/PersonControllerJsonTest.java
@@ -0,0 +1,111 @@
+package com.api.integrationstests.controller.withjson;
+
+import com.api.configs.TestConfigs;
+import com.api.integrationstests.httpbodies.CreateUpdatePartialsPersonRequest;
+import com.api.integrationstests.httpbodies.ResponsePerson;
+import com.api.integrationstests.testcontainers.AbstractIntegrationTest;
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class PersonControllerJsonTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+
+    private static ObjectMapper objectMapper;
+
+    private static CreateUpdatePartialsPersonRequest personFake;
+    private static ResponsePerson responsePerson;
+    private static Faker faker;
+
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES); //disable fails of the attributes without on object deserialization
+
+        personFake = new CreateUpdatePartialsPersonRequest();
+        responsePerson = null;
+        faker = new Faker();
+    }
+
+    @Test
+    @Order(1)
+    void createPersonTest() throws IOException {
+        mockPerson();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, "http://localhost:8080")
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        Response response = given().spec(specification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(personFake)
+                .when()
+                .post()
+                .then()
+                .extract()
+                .response();
+
+        responsePerson = objectMapper.readValue(response.body().asString(), ResponsePerson.class);
+
+        assertNotNull(responsePerson);
+
+        assertNotNull(responsePerson.getId());
+        assertNotNull(responsePerson.getFirstname());
+        assertNotNull(responsePerson.getLastname());
+        assertNotNull(responsePerson.getEmail());
+        assertNotNull(responsePerson.getUsername());
+        assertNotNull(responsePerson.getCreatedAt());
+        assertNotNull(responsePerson.getUpdatedAt());
+        assertNotNull(responsePerson.getBirthday());
+
+        assertTrue(responsePerson.getId().length() > 0);
+
+//        // TODO: verificar datas
+//        Date now = new Date();
+//        assertTrue(now.after(responsePerson.getCreatedAt()) || now.equals(responsePerson.getCreatedAt()));
+//        assertTrue(now.after(responsePerson.getUpdatedAt()) || now.equals(responsePerson.getUpdatedAt()));
+
+        assertEquals(personFake.getFirstname(), responsePerson.getFirstname());
+        assertEquals(personFake.getLastname(), responsePerson.getLastname());
+        assertEquals(personFake.getEmail(), responsePerson.getEmail());
+        assertEquals(personFake.getUsername(), responsePerson.getUsername());
+        assertEquals(personFake.getBirthday(), responsePerson.getBirthday());
+        assertEquals(personFake.getFirstname(), responsePerson.getFirstname());
+        assertEquals(personFake.getFirstname(), responsePerson.getFirstname());
+    }
+
+    private void mockPerson() {
+        String password = faker.internet().password();
+        personFake.setFirstname(faker.name().firstName());
+        personFake.setLastname(faker.name().lastName());
+        personFake.setEmail(faker.internet().emailAddress());
+        personFake.setPassword(password);
+        personFake.setPasswordConfirmation(password);
+        personFake.setBirthday(faker.date().birthday());
+        personFake.setUsername(faker.name().username());
+    }
+}
