92e1580d0b7afc4b6169cedcd8737017b8d5f0c5
navas <gabrielnavas@email.com>
2023-09-29 18:44:07 -0300

    init books tests

---
+++ person_and_book/src/test/java/com/api/integrationstests/controller/withjson/BookControllerJsonTest.java
@@ -0,0 +1,17 @@
+package com.api.integrationstests.controller.withjson;
+
+import org.junit.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class BookControllerJsonTest {
+
+
+    @Test
+    public void test() {
+        // TODO: implements the books tests
+    }
+}
