1df3433226b46fc31270515584445dc5184789f0
navas <gabrielnavas@email.com>
2023-10-01 15:30:41 -0300

    implements GrantedAuthority and create getters and setters

--- person_and_book/src/main/java/com/api/modules/person/models/Permission.java
+++ person_and_book/src/main/java/com/api/modules/person/models/Permission.java
@@ -3,12 +3,13 @@ package com.api.modules.person.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "permission")
-public class Permission {
+public class Permission implements GrantedAuthority {
 
 
     @Id
@@ -19,4 +20,28 @@ public class Permission {
     @Min(2)
     @Max(150)
     private String description;
+
+    public Permission() {
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
