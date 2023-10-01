0e6ee0f48ae862e35eabde79459e2dfc1e4780b6
navas <gabrielnavas@email.com>
2023-10-01 00:03:21 -0300

    fix the orders numbers

--- person_and_book/src/test/java/com/api/modules/book/integrationstests/controller/withjson/BookControllerJsonTest.java
+++ person_and_book/src/test/java/com/api/modules/book/integrationstests/controller/withjson/BookControllerJsonTest.java
@@ -95,8 +95,6 @@ public class BookControllerJsonTest extends AbstractIntegrationTest {
     @Test
     @Order(2)
     void createBookWithWrongOriginTest() {
-        bookFake = createNewBookRequest();
-
         specification = new RequestSpecBuilder()
                 .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_WRONG)
                 .setBasePath("/api/book/v1")
@@ -121,6 +119,74 @@ public class BookControllerJsonTest extends AbstractIntegrationTest {
 
     @Test
     @Order(3)
+    void getBookByIdTest() throws IOException {
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_LOCALHOST)
+                .setBasePath("/api/book/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        Response response = given().spec(specification)
+                .accept(ContentType.JSON)
+                .pathParam("bookId", responseBook.getId())
+                .when()
+                .get("{bookId}")
+                .then()
+                .extract()
+                .response();
+
+        ResponseBook responseBookFind = objectMapper.readValue(response.body().asString(), ResponseBook.class);
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+
+        assertNotNull(responseBookFind);
+
+        assertNotNull(responseBookFind.getId());
+        assertNotNull(responseBookFind.getAuthor());
+        assertNotNull(responseBookFind.getLaunchDate());
+        assertNotNull(responseBookFind.getTitle());
+        assertNotNull(responseBookFind.getPrice());
+
+        assertEquals(bookFake.getAuthor(), responseBookFind.getAuthor());
+        assertEquals(bookFake.getLaunchDate(), responseBookFind.getLaunchDate());
+        assertEquals(bookFake.getTitle(), responseBookFind.getTitle());
+        assertEquals(Math.ceil(bookFake.getPrice()), Math.ceil(responseBookFind.getPrice()));
+    }
+
+    @Test
+    @Order(4)
+    void getBookByIdWithWrongOriginTest() throws IOException {
+        CreateUpdatePartialsBookRequest bookToUpdate = createNewBookRequest();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_WRONG)
+                .setBasePath("/api/book/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        UUID bookFakeId = UUID.randomUUID();
+
+        Response response = given().spec(specification)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .pathParam("bookId", bookFakeId)
+                .body(bookToUpdate)
+                .when()
+                .get("{bookId}")
+                .then()
+                .extract()
+                .response();
+
+        assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatusCode());
+    }
+
+
+    @Test
+    @Order(5)
     void updatePartialsBookTest() throws IOException {
         CreateUpdatePartialsBookRequest bookToUpdate = createNewBookRequest();
 
@@ -148,7 +214,7 @@ public class BookControllerJsonTest extends AbstractIntegrationTest {
 
 
     @Test
-    @Order(4)
+    @Order(6)
     void updatePartialsBookWithWrongOriginTest() throws IOException {
         CreateUpdatePartialsBookRequest bookToUpdate = createNewBookRequest();
 
@@ -169,81 +235,6 @@ public class BookControllerJsonTest extends AbstractIntegrationTest {
                 .body(bookToUpdate)
                 .when()
                 .patch("{bookId}")
-                .then()
-                .extract()
-                .response();
-
-        assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatusCode());
-    }
-
-
-    @Test
-    @Order(4)
-    void getBookByIdTest() throws IOException {
-        CreateUpdatePartialsBookRequest bookToUpdate = createNewBookRequest();
-
-        specification = new RequestSpecBuilder()
-                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_LOCALHOST)
-                .setBasePath("/api/book/v1")
-                .setPort(TestConfigs.SERVER_PORT)
-                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
-                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
-                .build();
-
-        Response response = given().spec(specification)
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .pathParam("bookId", responseBook.getId())
-                .body(bookToUpdate)
-                .when()
-                .get("{bookId}")
-                .then()
-                .extract()
-                .response();
-
-        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
-
-        ResponseBook responseBook = objectMapper.readValue(response.body().asString(), ResponseBook.class);
-
-        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
-
-        assertNotNull(responseBook);
-
-        assertNotNull(responseBook.getId());
-        assertNotNull(responseBook.getAuthor());
-        assertNotNull(responseBook.getLaunchDate());
-        assertNotNull(responseBook.getTitle());
-        assertNotNull(responseBook.getPrice());
-
-        assertEquals(responseBook.getAuthor(), responseBook.getAuthor());
-        assertEquals(responseBook.getLaunchDate(), responseBook.getLaunchDate());
-        assertEquals(responseBook.getTitle(), responseBook.getTitle());
-        assertEquals(responseBook.getPrice(), responseBook.getPrice());
-    }
-
-
-    @Test
-    @Order(4)
-    void getBookByIdWithWrongOriginTest() throws IOException {
-        CreateUpdatePartialsBookRequest bookToUpdate = createNewBookRequest();
-
-        specification = new RequestSpecBuilder()
-                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_WRONG)
-                .setBasePath("/api/book/v1")
-                .setPort(TestConfigs.SERVER_PORT)
-                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
-                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
-                .build();
-
-        UUID bookFakeId = UUID.randomUUID();
-
-        Response response = given().spec(specification)
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .pathParam("bookId", bookFakeId)
-                .body(bookToUpdate)
-                .when()
-                .get("{bookId}")
                 .then()
                 .extract()
                 .response();
