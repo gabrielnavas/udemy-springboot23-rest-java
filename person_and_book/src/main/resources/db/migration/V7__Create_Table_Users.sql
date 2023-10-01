500c326b92805cb2c22c1e33ccd04b7a31e28994
navas <gabrielnavas@email.com>
2023-10-01 14:09:29 -0300

    change bit to boolean value

--- person_and_book/src/main/resources/db/migration/V7__Create_Table_Users.sql
+++ person_and_book/src/main/resources/db/migration/V7__Create_Table_Users.sql
@@ -3,10 +3,10 @@ CREATE TABLE IF NOT EXISTS users (
     user_name varchar(255) default null,
     full_name varchar(255) default null,
     password varchar(255) default null,
-    account_non_expired bit(1) default null,
-    account_non_locked bit(1) default null,
-    credentials_non_expired bit(1) default null,
-    enabled bit(1) default null,
+    account_non_expired boolean default null,
+    account_non_locked boolean default null,
+    credentials_non_expired boolean default null,
+    enabled boolean default null,
     CONSTRAINT tb_users_pk_id PRIMARY KEY (id),
     CONSTRAINT tb_users_unique_id UNIQUE (user_name)
 );
